2ce3b730d18132f2ed5dae8c97708444c7279356
Berend Buck <Florenz.Buck@web.de>
2025-05-01 20:07:45 +0200

    added InstructionReader.py, added Paths.py

---
+++ Paths.py
@@ -0,0 +1,6 @@
+from os import path
+
+main_path:str = path.dirname(__file__)
+input_path:str = main_path + "/input.txt"
+output_path:str = main_path + "/output.txt"
+instruction_path:str = main_path + "/files/instructions.txt"
