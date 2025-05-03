eda3cafbd818e513b77f455885253357df264951
Berend Buck <Florenz.Buck@web.de>
2025-05-03 16:06:11 +0200

    Fixed a bug in Lines.py::Line.line_to_hex() where hex values below 0x10 were one character long (3 counting the "0x"). Implemented a test in Main.py (modified input.txt and output.txt).

--- Lines.py
+++ Lines.py
@@ -11,4 +11,6 @@ class Line:
         register1_bin:str = '0' * (2 - len(ValueConverter.hex_to_bin(register1)[2::])) + ValueConverter.hex_to_bin(register1)[2::]
         register2_bin:str = '0' * (2 - len(ValueConverter.hex_to_bin(register2)[2::])) + ValueConverter.hex_to_bin(register2)[2::]
         
-        return ValueConverter.bin_to_hex(instruction_bin + register1_bin + register2_bin)
+        converted:str = ValueConverter.bin_to_hex(instruction_bin + register1_bin + register2_bin)
+        # assumes that the hex code never excedes 'ff'
+        return converted[:2] + '0' + converted[2::] if len(converted) == 3 else converted
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('test')
+import InputReader, Lines, OutputWriter
+
+TEST_SCRIPT:bool = False
+
+if __name__ == '__main__':
+    print('Assembling...')
+    
+    if TEST_SCRIPT:
+        file_lines:list[str] = InputReader.read_input_file()
+        hex_list:list[str] = []
+
+        for line in file_lines:
+            hex_list.append(Lines.Line(line).line_to_hex()[2::])
+        
+        output:str = OutputWriter.format_output_string(hex_list, True, True)
+        OutputWriter.write_output_file(output)
+        print(output) # To review output without having to open output.txt
+
+    print('Done!')
